efba6a41cf6062f501a65ed0bb5a235a0285a634
Your Name <you@example.com>
2018-10-22 21:45:24 +0530

    Updates

---
+++ precache-manifest.accabd1388d5ac4cf009b079468c887e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dbbb68ff22f5a09ad64154b105d62d6",
+    "url": "/thai-lime1/static/media/avatar.9dbbb68f.jpg"
+  },
+  {
+    "revision": "19de1e33c928d241d9b4",
+    "url": "/thai-lime1/static/js/runtime~main.19de1e33.js"
+  },
+  {
+    "revision": "5317536dbb59d0f9c5b0",
+    "url": "/thai-lime1/static/js/main.5317536d.chunk.js"
+  },
+  {
+    "revision": "6570cd55b9620671f6d8",
+    "url": "/thai-lime1/static/js/1.6570cd55.chunk.js"
+  },
+  {
+    "revision": "5317536dbb59d0f9c5b0",
+    "url": "/thai-lime1/static/css/main.79757bbe.chunk.css"
+  },
+  {
+    "revision": "6570cd55b9620671f6d8",
+    "url": "/thai-lime1/static/css/1.0a4ccda7.chunk.css"
+  },
+  {
+    "revision": "8cb70f7efca44239d6cf1d91a8deaa38",
+    "url": "/thai-lime1/index.html"
+  }
+];
